24997dc669350320951a074d1cf19736e0371f23
irinashvec999 <irinashvec999@gmail.com>
2018-10-25 14:36:37 +0300

    Task 2 - Done

--- src/com/hillel/hometask1/Start.java
+++ src/com/hillel/hometask1/Start.java
@@ -4,9 +4,9 @@ public class Start {
     public static void main(String[] args) {
         double a = Calculator.add(5.2, 7.3);
         System.out.println(" x + y =" + a);
-        double b = Calculator.sub(100.2, 35.1);
+        double b = Calculator.sub(100.2, 35D);
         System.out.println(" x - y =" + b);
-        double c = Calculator.div(200.4, 50.1);
+        double c = Calculator.div(200.4, 50D);
         System.out.println("x / y =" + c);
         double d = Calculator.mul(33.6, 3.3);
         System.out.println(" x * y =" + d);
