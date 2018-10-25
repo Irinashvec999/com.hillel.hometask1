32b1b9bed5f1a8fce0668c82f75b554b208c36fd
irinashvec999 <irinashvec999@gmail.com>
2018-10-25 13:33:26 +0300

    Task 1 -  Done

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public static int add(int x, int y){
+        int result =  x +  y;
+        return result;
+    }
+        public static int sub(int x, int y){
+            int result = x - y;
+            return result;
+        }
+        public static int div(int x,int y){
+        int result = x / y;
+        return result;
+        }
+        public static int mul(int x, int y){
+        int result = x * y;
+        return result;
+        }
+
+
+}
---
+++ src/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    public static void main(String[] args){
+       int a = Calculator.add(8,2);
+        System.out.println(" x + y =" + a);
+        int b = Calculator.sub(46,18);
+        System.out.println(" x - y =" + b);
+        int c = Calculator.div(72,8);
+        System.out.println("x / y =" + c);
+        int d = Calculator.mul(6,25);
+        System.out.println(" x * y =" + d);
+    }
+}
