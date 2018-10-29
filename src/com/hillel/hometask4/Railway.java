83d9a05ff3ab8c7bc6e639ed46c56a14a0297272
irinashvec999 <irinashvec999@gmail.com>
2018-10-29 16:58:47 +0200

    Task 4.4 - Done

---
+++ src/com/hillel/hometask4/Railway.java
@@ -0,0 +1,23 @@
+package com.hillel.hometask4;
+
+public class Railway {
+    public static int Survivingtime1(int x){
+        int t1 = 4000 / x; //how long does the train1 drives 4m
+        return t1;
+    }
+    public static int Survivingtime2 (int y){
+        int t2 = 6000 / y;//how long does the train2 drives 6m
+        return t2;
+}
+public static void main(String[] args) {
+    int h = Railway.Survivingtime1(50);
+    int h2 = Railway.Survivingtime2(50);
+    if (h > h2) {
+        System.out.println(" Railway accident!!!");
+    } else if (h == h2) {
+        System.out.println(" Railway accident!!!");
+    } else {
+        System.out.println("Yippee! Everyone saved! ");
+    }
+    }
+}
