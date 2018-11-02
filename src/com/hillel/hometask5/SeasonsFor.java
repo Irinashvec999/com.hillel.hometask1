09082015a8a1a42d092a54c8a7810e2f08feb624
irinashvec999 <irinashvec999@gmail.com>
2018-11-02 13:42:58 +0200

    Task 5 - Corrections

--- src/com/hillel/hometask5/SeasonsFor.java
+++ src/com/hillel/hometask5/SeasonsFor.java
@@ -9,8 +9,10 @@ public class SeasonsFor {
             System.out.println(" Summer ");
         }else if (9 <= x && x <= 11){
             System.out.println(" Outumn ");
-        }else{
+        }else if (x==12 && x==1 && x==2){
             System.out.println(" Winter ");
+        }else {
+            System.out.println(" There is no such season of the year ");
         }
     }
 }
