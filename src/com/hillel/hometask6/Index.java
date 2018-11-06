3c4c2e2c614b47c6b58d5509b55ff54b269e87d6
Irinashvec999 <irinashvec999@gmail.com>
2018-11-07 00:51:04 +0200

    Task 6 -Done

--- src/com/hillel/hometask6/Index.java
+++ src/com/hillel/hometask6/Index.java
@@ -3,14 +3,21 @@ package com.hillel.hometask6;
 public class Index {
     public static void main(String[] args) {
         String inStr = "Одесса мой город родной";
-        int len = inStr.length();
-        int indexO = inStr.indexOf("о"); // 8
-        char f = inStr.charAt(indexO); // o
+        int indFirst = inStr.indexOf("о"); // 8
+        int indLast = inStr.lastIndexOf("о");
 
-        for (indexO = 0; indexO <= len; indexO++) {
-            int lookForInd = inStr.indexOf(f, indexO + 1);//looking for "o" from 8
-            System.out.print(lookForInd + " ");
+        for (int i = indFirst; i <= indLast; i++) {
+            char i1 = inStr.charAt(i);
+            if (i1 == 'о') {
+                    System.out.print(inStr.indexOf("о",i) + " ");
+            } else {
+                i = i++;
+            }
         }
     }
 }
+
+
+
+
 
