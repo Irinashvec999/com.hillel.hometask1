0f8e7c000af9cc6c39147042e98fed16a8423477
Dalliya <dalliyal@gmail.com>
2018-11-07 17:16:47 +0200

    Square done

--- src/com/hillel/hometask6/Square.java
+++ src/com/hillel/hometask6/Square.java
@@ -1,15 +1,19 @@
 package com.hillel.hometask6;
 
-import com.hillel.Utils.MathUtil;
 
 public class Square {
     public static void main(String[] args) {
-                for (int row=0; row <5; row++){
-                for (int column=0; column <1; column++){
-                    System.out.print("1"+"0"+"0"+"0"+"0"+"0"+"0");
+        for (int row = 0; row < 5; row++) {
+            for (int column = 0; column < 5; column++) {
+                if (row == column) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
                 }
-                System.out.println();
             }
+            System.out.println();
         }
     }
+}
+
 
