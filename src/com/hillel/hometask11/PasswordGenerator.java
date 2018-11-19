0f31594885e531914a21cd82b0707377c37b9e46
irinashvec999 <irinashvec999@gmail.com>
2018-11-19 12:30:33 +0200

    Task 11 - corrected password

--- src/com/hillel/hometask11/PasswordGenerator.java
+++ src/com/hillel/hometask11/PasswordGenerator.java
@@ -1,22 +1,62 @@
 package com.hillel.hometask11;
-
 import com.hillel.Utils.MathUtil;
+
+import java.util.Arrays;
 
 
 public class PasswordGenerator {
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String UNDERSCORE = "_";
 
     public static void main(String[] args) {
-        String symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        char symbol = 0;
-        int maxLength = 8;
-
-        for (int i = 0; i < maxLength; i++) {
-            for (int j = 0; j < maxLength; j++) {
-                j = MathUtil.getRandom(0, 62);
-                char x = symbols.charAt(j);
-                symbol = x;
+        char [] password = new char[]{findRandomUpper(),findRandomLower(),findRandomDigits(),findUnderscore(),findRandomLower(),findRandomUpper(),findRandomDigits(),findRandomUpper()};
+//Let's doing password unique
+        int counter = 0;
+        for (int rand = 0; rand < password.length; rand++) {
+            rand = MathUtil.getRandom(0,password.length-1);
+            if (isUnique (rand,password)){
+                password[counter]= password[rand];
+                counter++;
             }
-            System.out.print(symbol);
         }
+        System.out.println(password);
     }
+
+    public static boolean isUnique(int rand, char[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if (i == rand){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static char findRandomUpper() {
+        int randomUpper = MathUtil.getRandom(0, UPPER.length()-1);
+        char upperSymbol = UPPER.charAt(randomUpper);
+        return upperSymbol;
+    }
+
+    public static char findRandomLower() {
+        int randomLower = MathUtil.getRandom(0, LOWER.length()-1);
+        char lowerSymbol = LOWER.charAt(randomLower);
+        return lowerSymbol;
+    }
+
+    public static char findRandomDigits() {
+        int randomDigits = MathUtil.getRandom(0, DIGITS.length()-1);
+        char digits = DIGITS.charAt(randomDigits);
+        return digits;
+    }
+
+    public static char findUnderscore() {
+        char underscore = UNDERSCORE.charAt(0);
+        return underscore;
+    }
+
+
+
 }
